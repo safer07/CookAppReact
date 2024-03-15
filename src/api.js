672807a25a0b4b0866928feb4c1c0f92717d31d7
safer07 <b0p0halex@gmail.com>
2024-03-15 19:05:16 +0300

    Загрузка рецептов через mockAPI

--- src/api.js
+++ src/api.js
@@ -8,10 +8,26 @@ export function fetchCategories() {
   });
 }
 
-export function fetchRecipes() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(recipes);
-    }, 100);
-  });
+// export function fetchRecipes() {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve(recipes);
+//     }, 100);
+//   });
+// }
+
+export async function fetchRecipes() {
+  // return fetch("https://65f16da8034bdbecc7628a2a.mockapi.io/recipes")
+  //   .then((response) => response.json())
+  //   .catch((error) => {
+  //     console.error(error);
+  //   });
+  try {
+    const url = "https://65f16da8034bdbecc7628a2a.mockapi.io/recipes";
+    const response = await fetch(url);
+    const data = response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
 }
